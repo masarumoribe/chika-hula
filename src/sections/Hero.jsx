d1bddf10f1f289ed10b22a06733a6d63c1e168f5
Masaru <user@USERnoMacBook-Pro.local>
2024-09-16 17:47:43 +0900

    Updated the mobile version hero section, temporary

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -7,22 +7,24 @@ const Hero = () => {
   return (
     <section
     id="home"
-    className="w-full flex flex-col justify-center min-h-screen bg-cyan-400 overflow-hidden">
-      <div className="relative flex flex-col justify-center items-start w-full z-10 px-5">
-        <p className="absolute text-2xl font-montserrat font-semibold text-amber-300 whitespace-nowrap -top-[310px]">Aloha Nani Pua Melia Chika <br/> Hula Academy</p>
-        <h1 className="absolute font-palanquin text-[35px] leading-10 font-bold whitespace-nowrap -top-[130px]">
-          <span className="relative bg-cyan-400 opacity-85 pr-10 rounded-r-[50px]">Enjoy Our New</span>
+    className=" relative w-full flex flex-col justify-center items-center min-h-screen bg-cyan-400 overflow-hidden">
+      <div className="absolute flex flex-col justify-between items-center w-full h-full z-10 p-10">
+        <p className="text-xl font-montserrat font-semibold text-amber-300 whitespace-nowrap pt-24">Aloha Nani Pua Melia Chika <br/> Hula Academy</p>
+        <h1 className="flex flex-col justify-center items-center font-palanquin text-[35px] leading-10 font-bold whitespace-nowrap mt-40">
+          <div className="bg-cyan-400 opacity-85 px-10 rounded-[50px] py-3">Enjoy Our New</div>
           <br />
-          <span className="text-amber-300 inline-block mt-3">Hula</span> Classes
+          <div className="bg-cyan-400 opacity-85 px-10 rounded-[50px] py-3"><span className="text-amber-300 inline-block">Hula</span> Classes</div>
         </h1>
-        <p className="absolute font-montserrat text-slate-gray text-sm leading-5 -bottom-[380px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
-        <Button label="Enroll now" iconURL={arrowRight}/>
+        <p className="hidden font-montserrat text-slate-gray text-sm leading-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+        <div className="">
+          <Button label="Enroll now" iconURL={arrowRight}/>
+        </div>
       </div>
-      <div className="absolute w-11/12 -right-20 flex justify-center items-center overflow-hidden pt-20">
+      <div className="absolute w-10/12 flex justify-center items-center overflow-hidden mt-20">
         <img
         src={heroPhoto}
         alt="hula dance"
-        className="object-cover rounded-l-[30px] rounded-br-[30px]"/>
+        className="object-cover rounded-[30px]"/>
       </div>
     </section>
   )
